a0fb4120c8782f953bc295517276ee7d46977301
Olga <123354636+blackOlga@users.noreply.github.com>
2023-01-25 05:02:28 +0300

    Initial commit

---
+++ src/com/senla/task2/Night.java
@@ -0,0 +1,18 @@
+package com.senla.task2;
+
+public class Night {
+    public static void main(String[] args) {
+        int s = 215;
+        byte j = 125;
+        short l = 1500;
+        long f = 2000;
+        public void test() {
+            Integer integer = 215;
+            Byte aByte = 125;
+            Short aShort = 1500;
+            System.out.println(s);
+
+
+        }
+    }
+}
---
+++ src/com/senla/task2/Task2.java
@@ -0,0 +1,30 @@
+package com.senla.task2;
+
+public class Task2 {
+   byte d = 100;
+    short e = 12000;
+    int k = 185;
+    long h = 1500000;
+    float m = 1.15F;
+    double v = 153.25;
+    boolean g = true;
+
+
+    public static void main(String[] args) {
+        Byte  d =100;
+        Short e = 12000;
+        Integer k = 185;
+        Long h = 1500000L;
+        Float m= 1.15F;
+        Double v = 153.25;
+        Boolean g = true;
+
+
+        System.out.println(d+k);
+
+
+    }
+
+    }
+
+
