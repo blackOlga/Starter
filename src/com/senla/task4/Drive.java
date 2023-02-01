50b268c251817ff5302d37ab1a551d32343f5939
Olga <123354636+blackOlga@users.noreply.github.com>
2023-02-01 14:59:11 +0300

    домашнее задание Cat

---
+++ src/com/senla/task4/Drive.java
@@ -0,0 +1,103 @@
+package com.senla.task4;
+
+import java.util.Random;
+
+
+public class Drive {
+
+
+    public static void menu() {
+        String[] arrayCats = new String[]
+                {"Бант", "Даша", "Пушок", "Айда", "Барсик", "Лорд", "Тишка", "Тоша", "Дружок", "Мурка"};
+        int length = arrayCats.length;
+        int ran = (int) Math.floor(Math.random() * length);
+        int age = (int) Math.floor(Math.random() * 28);
+        String phrase = arrayCats[ran] + " " + age;
+         int i = 4;
+        do {
+            System.out.println("Имя и возраст котёнка : " + phrase);
+        }while ( i > 5);
+    }
+
+
+    static void Start() {
+        String[] arrayCats = new String[]
+                {"Бант", "Даша", "Пушок", "Айда", "Барсик", "Лорд", "Тишка", "Тоша", "Дружок", "Мурка"};
+
+        int length = arrayCats.length;
+        int ran = (int) Math.floor(Math.random() * length);
+        int age = (int) Math.floor(Math.random() * 28);
+
+            String phrase = arrayCats[ran] + " " + age;
+            int i = 3;
+            while (i < 2) ;
+
+            System.out.println("Имя котёнка и возраст : " + phrase);
+            --i;
+        }
+
+    static void example1() {
+        String[] arrayCats = new String[]
+                {"Бант", "Даша", "Пушок", "Айда", "Барсик", "Лорд", "Тишка", "Тоша", "Дружок", "Мурка"};
+
+        int length = arrayCats.length;
+        int ran = (int) Math.floor(Math.random() * length);
+        int age = (int) Math.floor(Math.random() * 28);
+
+        String phrase = arrayCats[ran] + " " + age;
+
+        for (int j = 10; j > 5; j--) {
+            System.out.println("Имя котёнка и возраст : " + phrase);
+        }
+    }
+
+    static void example2() {
+        String[] arrayCats = new String[]{"Бант", "Даша", "Пушок", "Айда", "Барсик"};
+        int length = arrayCats.length;
+        int ran = (int) Math.floor(Math.random() * length);
+        int age = (int) Math.floor(Math.random() * 28);
+        String phrase = arrayCats[ran] + " " + age;
+
+        for (String i : arrayCats) {
+            System.out.println(i);}
+            {
+                System.out.println("Имя котёнка и возраст : " + phrase);
+            }
+
+        }
+
+    }
+
+
+
+
+
+
+
+
+      //  Cat cat = new Cat("Мурка", 21);
+      //  new Cat("Даша", 5);
+      //  new Cat("Айда", 1);
+      //  new Cat("Снежок", 4);
+      //  new Cat("Барсик", 3);
+      //  new Cat("Лорд", 2);
+      //  new Cat("Тишка", 6);
+      //  new Cat("Тоша", 15);
+      //  new Cat("Дружок", 7);
+      //  new Cat("Мурка", 12);
+      //  Random random = new Random();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
